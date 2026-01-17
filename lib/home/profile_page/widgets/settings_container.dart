0cdb6920afc4e365a4d89890a9fac076975fe85b
YehiaHany22 <yehiahany091@gmail.com>
2026-01-17 06:05:45 +0200

    feat(ui):slight change in container height in settings container widget.

--- lib/home/profile_page/widgets/settings_container.dart
+++ lib/home/profile_page/widgets/settings_container.dart
@@ -25,7 +25,7 @@ class _SettingsContainerState extends State<SettingsContainer> {
             themeProvider.isDarkMode() ? ThemeMode.light : ThemeMode.dark);
       },
       child: Container(
-        height: 47,
+        height: 48,
         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 0 /*16*/),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
