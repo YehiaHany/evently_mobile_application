4d1a3bfb56d8f8d28eeae068793b30a33eef435f
YehiaHany22 <yehiahany091@gmail.com>
2026-01-19 09:39:09 +0200

    feat(fix):issues resolved from resolving conflicts during the onboarding screen merge.

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -49,6 +49,7 @@ class AppStyles{
     fontSize: 17,
     fontWeight: FontWeight.w400,
     color: AppColors.grayBorder,
+  );
   static final TextStyle regular12PrimaryBlue = GoogleFonts.poppins(
     fontSize: 12,
     fontWeight: FontWeight.w400,
