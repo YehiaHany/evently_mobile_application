2847c2b2f09f753e7671cdcd74caf49980f468ff
YehiaHany22 <yehiahany091@gmail.com>
2026-02-14 16:25:14 +0200

    feat(logic):created user logic. +user model. +firebase set and get new user. +adjusted authentication to new logic.

--- lib/firebase_utils.dart
+++ lib/firebase_utils.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:evently/model/my_user.dart';
 
 import 'model/event_model.dart';
 
@@ -13,10 +14,29 @@ class FirebaseUtils {
         );
   }
 
+  static CollectionReference<MyUser> getUserCollection() {
+    return FirebaseFirestore.instance
+        .collection(MyUser.collectionName)
+        .withConverter<MyUser>(
+          fromFirestore:
+              (snapshot, _) => MyUser.fromFireStore(snapshot.data()!),
+          toFirestore: (user, options) => user.toFireStore(),
+        );
+  }
+
   static Future<void> addEventToFireStore(EventModel event) {
     CollectionReference<EventModel> collectionReference = getEventsCollection();
     DocumentReference<EventModel> doc = collectionReference.doc();
     event.id = doc.id;
     return doc.set(event);
+  }
+
+  static Future<void> addUserToFireStore(MyUser user) {
+    return getUserCollection().doc(user.id).set(user);
+  }
+
+  static Future<MyUser?> getUserFromFireStore(String uId) async {
+    var querySnapShot = await getUserCollection().doc(uId).get();
+    return querySnapShot.data();
   }
 }
